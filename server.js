2ed9dd916c217c6d5e90a5d3dd52ecea2f56ef22
Yelena <88336329+DYA13@users.noreply.github.com>
2023-05-17 10:07:07 +0200

    new file:   middleware/message.js,modified:   server.js

--- server.js
+++ server.js
@@ -1,30 +1,45 @@
-var express = require('express');
+require("dotenv").config();
+const connectDB = require("./db/connect");
+const session = require("express-session");
+var express = require("express");
 var app = express();
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // use res.render to load up an ejs view file
 
 // index page
-app.get('/', function(req, res) {
+app.get("/", function (req, res) {
   var mascots = [
-    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-    { name: 'Tux', organization: "Linux", birth_year: 1996},
-    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    { name: "Sammy", organization: "DigitalOcean", birth_year: 2012 },
+    { name: "Tux", organization: "Linux", birth_year: 1996 },
+    { name: "Moby Dock", organization: "Docker", birth_year: 2013 },
   ];
-  var tagline = "No programming concept is complete without a cute animal mascot.";
+  var tagline =
+    "No programming concept is complete without a cute animal mascot.";
 
-  res.render('pages/index', {
+  res.render("pages/index", {
     mascots: mascots,
-    tagline: tagline
+    tagline: tagline,
   });
 });
 
 // about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
+app.get("/about", function (req, res) {
+  res.render("pages/about");
 });
 
-app.listen(8080);
-console.log('Server is listening on port 8080');
+const port = 8080;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
